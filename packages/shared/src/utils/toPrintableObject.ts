5ebe49889029bc0aae68bed03065f2165fa0974d
Dominic Tobias <dominic@inkfnd.com>
2026-01-23 12:35:41 +0400

    Improve accuracy of toPrintableObject return type

--- packages/shared/src/utils/toPrintableObject.ts
+++ packages/shared/src/utils/toPrintableObject.ts
@@ -4,6 +4,12 @@ import { BigDecimal } from './math/bigDecimal';
  * Util for converting any BigDecimal types into a string so that it can be logged nicely.
  * Handles cyclic references by returning '[Circular]' for already-visited objects.
  */
+export function toPrintableObject(obj: null, seen?: WeakSet<object>): null;
+export function toPrintableObject(
+  obj: BigDecimal | bigint,
+  seen?: WeakSet<object>,
+): string;
+export function toPrintableObject(obj: unknown, seen?: WeakSet<object>): object;
 export function toPrintableObject(obj: unknown, seen = new WeakSet()): unknown {
   if (obj == null) {
     return null;
