254b1af0ab9409bfcc5dc5bb3c056c1f632a78be
Dominic Tobias <dominic@inkfnd.com>
2026-01-27 22:31:31 +0100

    Remove unused code

--- setup-links.sh
+++ setup-links.sh
@@ -3,14 +3,5 @@
 # Check UNLINK env var as a quick way to use a "flag"
 if [ "$UNLINK" ]; then BUN_CMD="bun unlink"; else BUN_CMD="bun link"; fi
 
-for DIR in "${DIRECTORIES[@]}"; do
-  # Change to the directory
-  cd "$DIR"
-  # Run bun link/unlink
-  $BUN_CMD
-  # Go back to the original directory
-  cd -
-done
-
 # Now link monorepo packages
 bunx lerna exec -- $BUN_CMD
