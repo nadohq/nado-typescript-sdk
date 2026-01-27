77c2a2559a1b3e8799a9e396d564753db2a2222b
Dominic Tobias <dominic@inkfnd.com>
2026-01-27 22:41:14 +0100

    Remove withPeerDepsExternal

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -1,4 +1,3 @@
 import baseConfig from '../../tsup.base.config';
-import { withPeerDepsExternal } from '../../tsup.utils';
 
-export default withPeerDepsExternal(import.meta.url, baseConfig);
+export default baseConfig;
