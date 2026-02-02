ff8281e0c7b21bddcfcb90b7813e8f62be4b9761
Dominic Tobias <dominic@inkfnd.com>
2026-02-03 00:42:22 +0100

    Add back viem

--- setup-links.sh
+++ setup-links.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# These libraries require a singular source of truth for library code, so having multiple instances
+# will break local development when linked with web
+DIRECTORIES=(
+  "node_modules/viem"
+)
+
 # Check UNLINK env var as a quick way to use a "flag"
 if [ "$UNLINK" ]; then BUN_CMD="bun unlink"; else BUN_CMD="bun link"; fi
+
+for DIR in "${DIRECTORIES[@]}"; do
+  # Change to the directory
+  cd "$DIR"
+  # Run yarn link/unlink
+  $BUN_CMD
+  # Go back to the original directory
+  cd -
+done
 
 # Now link monorepo packages
 bunx lerna exec -- $BUN_CMD
